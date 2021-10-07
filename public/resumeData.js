6f3b25e6ec2f085e390e39b5d4fbae3155d9b2aa
Matt Bongiorno <bigiorno@gmail.com>
2021-10-07 12:19:04 -0400

    resume data skeleton

---
+++ public/resumeData.js
@@ -0,0 +1,45 @@
+let resumeData = {
+    "imagebaseurl": "https://rbhatia46.github.io/",
+    "name": "Matthew Bongiorno",
+    "role": "Developer",
+    "linkedinId": "MattBongiorno",
+    "twitterId": "Your twitterId",
+    "roleDescription": "Full stack web developer, startup co-founder, problem solver",
+    "socialLinks": [{
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/mattbongiorno/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "http://github.com/MattBongiorno",
+            "className": "fa fa-github"
+        },
+        {
+            "name": "twitter",
+            "url": "http://twitter.com/CoolReduct",
+            "className": "fa fa-twitter"
+        }
+    ],
+    "aboutme": "",
+    "address": "Beverly Massachusetts",
+    "website": "",
+    "education": [{
+            "UniversityName": "University of New Hnmpshire",
+            "specialization": "Full-stack web development",
+            "MonthOfPassing": "Dec",
+            "YearOfPassing": "2020",
+            "Achievements": ""
+        },
+        {
+            "UniversityName": "Some University",
+            "specialization": "Some specialization",
+            "MonthOfPassing": "Jan",
+            "YearOfPassing": "2018",
+            "Achievements": "Some Achievements"
+        }
+    ],
+
+}
+
+export default resumeData
